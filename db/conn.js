db1c5186c763582b8e35ad563ec2d30aa2c79314
aldeiadocafe <aldeiadocafe@aldeiadocafe.com.br>
2026-03-31 16:47:11 -0300

    Atualizando .gitignore

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+const uri = process.env.MONGODB_URI
+
+async function main(){
+
+//    await mongoose.connect("mongodb://localhost:27017/aldeia?replicaSet=rs0"); //O nome rs0 está no arquivo C:\Program Files\MongoDB\Server\<version>\bin\mongod.cfg
+    await mongoose.connect(uri)
+
+    console.log("conectou ao Mongoose");
+}
+
+main().catch((err) => console.log(err));
+
+module.exports = mongoose;
