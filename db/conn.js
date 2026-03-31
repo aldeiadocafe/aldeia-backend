499c58ca1fb9eb52b301efb81097bf04133a33e7
aldeiadocafe <aldeiadocafe@aldeiadocafe.com.br>
2026-03-31 17:05:07 -0300

    As variaveis do .env devem ser criadas  como variaveis de ambiente na Vercel

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI
 
 async function main(){
-console.log(uri)
+
 //    await mongoose.connect("mongodb://localhost:27017/aldeia?replicaSet=rs0"); //O nome rs0 está no arquivo C:\Program Files\MongoDB\Server\<version>\bin\mongod.cfg
     await mongoose.connect(uri)
 
